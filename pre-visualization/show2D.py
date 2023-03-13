bdb47c050ebd87db3a4ed4a37c07c2bd2801c661
Sherraytt <lei_xiaaa@163.com>
2023-03-13 08:02:06 +0800

    remove outlier

--- pre-visualization/show2D.py
+++ pre-visualization/show2D.py
@@ -1,13 +1,24 @@
 import matplotlib.pyplot as plt 
 import pandas as pd 
+import numpy as np
 import os
+
+def save_img(_df, _col, _figTitle, _filename):
+    _df.plot.scatter('x1', 'x2', c=_col, s=1)
+    plt.gcf().set_size_inches(8, 6)
+    plt.title(_figTitle)              
+    plt.xlim((x1lim_min, x1lim_max))
+    plt.ylim((x2lim_min, x2lim_max))
+    plt.savefig(_filename, bbox_inches='tight', dpi=300)
+    plt.close()
+
 
 if __name__ == '__main__':
     col_all = []
     col_domain = []
     col_others = []
     # blue, red, gray
-    colors = ['#0000FF', '#FF0000', '#696969']
+    colors = ['#0000FF', '#FF0000', '#D3D3D3']
     df = pd.read_csv('middle/single-words_PCA_2d.csv')
     labels = df.label.values
 
@@ -45,29 +56,38 @@ if __name__ == '__main__':
                     x2lim_max = max(x2s)
                     x2lim_max = x2lim_max + abs(x2lim_max) / 10
                     
-                    plt.xlim((x1lim_min, x1lim_max))
-                    plt.xyim((x2lim_min, x2lim_max))
-                    df.plot.scatter('x1', 'x2', c=col_all, s=10)
-                    plt.gcf().set_size_inches(1, 1)
-                    plt.title(pre + '_' + rd + 'points all')              
-                    plt.savefig('img/' + pre + '_' + rd + '_all.jpg', bbox_inches='tight', dpi=300)
-                    plt.close()
-
+                    save_img(df, col_all, pre + '_' + rd + 'points all', 'img/' + pre + '_' + rd + '_all.jpg')
                     df_domain = df.loc[df['label'] != -1]
-                    plt.xlim((x1lim_min, x1lim_max))
-                    plt.xyim((x2lim_min, x2lim_max))
-                    df_domain.plot.scatter('x1', 'x2', c=col_domain, s=10)
-                    plt.gcf().set_size_inches(1, 1)
-                    plt.title(pre + '_' + rd + 'points domain')
-                    plt.savefig('img/' + pre + '_' + rd + '_domain.jpg', bbox_inches='tight', dpi=300)
-                    plt.close()
-
+                    save_img(df_domain, col_domain, pre + '_' + rd + 'points domain', 'img/' + pre + '_' + rd + '_domain.jpg')
                     df_others = df.loc[df['label'] == -1]
-                    plt.xlim((x1lim_min, x1lim_max))
-                    plt.xyim((x2lim_min, x2lim_max))
-                    df_others.plot.scatter('x1', 'x2', c=col_others, s=10)
-                    plt.gcf().set_size_inches(1, 1)s
-                    plt.title(pre + '_' + rd + 'points others')
-                    plt.savefig('img/' + pre + '_' + rd + '_others.jpg', bbox_inches='tight', dpi=300)
-                    plt.close()
+                    save_img(df_others, col_others, pre + '_' + rd + 'points others', 'img/' + pre + '_' + rd + '_others.jpg')
 
+                    if 'concat-words_LLE' in filename:
+                        x2_mean = np.mean(x2s)
+                        x2_std = np.std(x2s)
+                        _3_lower = x2_mean - 3 * x2_std
+                        _3_upper = x2_mean + 3 * x2_std
+                        
+                        # remove outlier
+                        df_domain_ = df_domain.loc[(_3_lower < df_domain['x2']) & (df_domain['x2'] < _3_upper)]
+                        df_others_ = df_others.loc[(_3_lower < df_others['x2']) & (df_others['x2'] < _3_upper)]
+                        df_ = pd.concat([df_domain_, df_others_])
+
+                        col_domain_ = col_domain[len(df_domain) - len(df_domain_):]
+                        col_others_ = col_others[len(df_others) - len(df_others_):]
+                        col_all_ = col_domain_ + col_others_
+
+                        x1s = df_.x1.values
+                        x2s = df_.x2.values
+                        x1lim_min = min(x1s)
+                        x1lim_min = x1lim_min - abs(x1lim_min) / 10
+                        x1lim_max = max(x1s)
+                        x1lim_max = x1lim_max + abs(x1lim_max) / 10
+                        x2lim_min = min(x2s)
+                        x2lim_min = x2lim_min - abs(x2lim_min) / 10
+                        x2lim_max = max(x2s)
+                        x2lim_max = x2lim_max + abs(x2lim_max) / 10
+                        
+                        save_img(df_, col_all_, pre + '_' + rd + ' points all processed', 'img/' + pre + '_' + rd + '_all_processed.jpg')
+                        save_img(df_domain_, col_domain_, pre + '_' + rd + ' points domain processed', 'img/' + pre + '_' + rd + '_domain_processed.jpg')
+                        save_img(df_others_, col_others_, pre + '_' + rd + ' points others processed', 'img/' + pre + '_' + rd + '_others_processed.jpg')
